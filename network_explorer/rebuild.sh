863a735c22113bc2a1bc8dee4cb4c01a3d00a951
Mohan Gupta <raspberrypisig@gmail.com>
2020-08-13 09:49:49 +1000

    web changes and build process

--- network_explorer/rebuild.sh
+++ network_explorer/rebuild.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo "Copy files to right directory."
 mkdir -p /home/pi/homeassistant/share/hassio/addons/local/network_explorer
-rsync -avu /home/pi/Developer/homeassistant-addons/network_explorer /home/pi/homeassistant/share/hassio/addons/local
+rsync -avu --exclude 'web'  /home/pi/Developer/homeassistant-addons/network_explorer /home/pi/homeassistant/share/hassio/addons/local
+rsync -av /home/pi/Developer/homeassistant-addons/network_explorer/web/build/ /home/pi/homeassistant/share/hassio/addons/local/network_explorer/rootfs/html
 echo "Stopping existing addon."
 ha addons stop local_network_explorer
 echo "Rebuilding addon."
