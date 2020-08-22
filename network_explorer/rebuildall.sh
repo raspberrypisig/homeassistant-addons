b453c4ccf5ec6e730e3c080be8a3c12de1c175b1
Mohan Gupta <raspberrypisig@gmail.com>
2020-08-22 13:19:09 +1000

    network_explorer:  rsync for git

--- network_explorer/rebuildall.sh
+++ network_explorer/rebuildall.sh
@@ -8,6 +8,7 @@ echo "Copy files to right directory."
 mkdir -p /home/pi/homeassistant/share/hassio/addons/local/network_explorer
 rsync -avu --exclude 'web'  /home/pi/Developer/homeassistant-addons/network_explorer /home/pi/homeassistant/share/hassio/addons/local
 rsync -av /home/pi/Developer/homeassistant-addons/network_explorer/web/build/ /home/pi/homeassistant/share/hassio/addons/local/network_explorer/rootfs/html
+rsync -av /home/pi/Developer/homeassistant-addons/network_explorer/web/build/ /home/pi/Developer/homeassistant-addons/network_explorer/rootfs/html/
 echo "Stopping existing addon."
 ha addons stop local_network_explorer
 echo "Rebuilding addon."
